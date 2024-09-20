ee649764b556b2ed332127091b197bbabaf6b90b
Muthuraj <muthuraj.k@quadkast.co.in>
2024-09-20 21:10:20 +0530

    L

--- src/inventory/routes.js
+++ src/inventory/routes.js
@@ -2,7 +2,7 @@ import Inventory from "./pages/inventory";
 import Goods from "./pages/goods";
 
 export const routes=[
-{path:'', element:<Inventory/>},
+{path:'', element:<Goods/>},
 {path:'inventory', element:<Inventory/>},
 {path:'*', element:<Goods/>},
 {path:'goods-receive-note', element:<Goods/>},
